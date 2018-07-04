f4098922a3db2c1eba129aa5609638f6c1c3b33e
Fluffhead88 <thigpen.zach@gmail.com>
2018-07-04 14:07:42 -0400

    ready for review

--- main.js
+++ main.js
@@ -2,19 +2,23 @@
 // Show me how to calculate the average price of all items. Please console.log the average.
 // The average price is $23.63
 
+// creates array of just prices
 var priceArray = items.map(function(item) {
   return item.price;
 })
 
-
+// adds all prices together
 var priceTotal = priceArray.reduce(function(accum, i) {
   return accum + i;
 });
 
+// divides prices by the length or total number of prices
 dec_answer = priceTotal / priceArray.length
 
+// truncates decimal to 2 places
 answer = dec_answer.toFixed(2)
 
+// prints the answer
 console.log("The average price is $" +
   answer)
 
@@ -36,10 +40,13 @@ console.log("The average price is $" +
 //    }
 //   ]
 
+// filters items that are between the price range
 var x = items.filter(function(item) {
   return item.price > 13.99 && item.price < 18.01;
 });
 console.log("Items that cost between $14.00 USD and $18.00 USD")
+
+// add title
 x.forEach(function(item) {
   console.log(item.title + '\n');
 });
@@ -48,9 +55,12 @@ x.forEach(function(item) {
 // Show me how find the item with a "GBP" currency code and print its name and price. Please console.log the one you find.
 // 1970s Schlitz Malt Liquor Glass Beer Pitcher costs £18
 
+// filters items based on currency code and targets GBP
 var brit = items.filter(function(item) {
   return item.currency_code === "GBP";
 });
+
+// prints title and the price
 brit.forEach(function(item) {
   console.log(item.title + " £" +
     item.price);
@@ -64,13 +74,15 @@ brit.forEach(function(item) {
 // Magnetic Wall Mount Bottle Opener Barware Set - Stainless Steel or Black - Personalized if you like! is made of wood.
 // Engraved Pocket Knife, Personalized Groomsmen Gift, Ring Bearer Gift, Graduation Gift, 4 Knives is made of wood.
 
-// var wood = items.filter(function(item) {
-//   return item.materials == "wood";
-// });
-// console.log(wood)
-// wood.forEach(function(item) {
-//   console.log(item.descrition);
-// });
+// filters materials based on an index that includes wood
+var wood = items.filter(function(item) {
+  return item.materials.indexOf("wood") != -1;
+});
+
+// adds is made of wood to title
+wood.forEach(function(item) {
+  console.log(item.title + "is made of wood");
+});
 
 // var materials = items.forEach(function(item) {
 //   console.log(item.materials);
@@ -107,9 +119,11 @@ brit.forEach(function(item) {
 // the three broomsticks glass
 // personalized harry potter glass
 
+// filters items with materials that have a length more than 8
 var x = items.filter(function(item) {
   return item.materials.length >= 8;
 });
+// prints title and materials
 x.forEach(function(item) {
   console.log(item.title + item.materials);
 });
@@ -118,7 +132,9 @@ x.forEach(function(item) {
 // Show me how to calculate how many items were made by their sellers. Please console.log your answer.
 // 18 were made by their sellers
 
+// filters items by who made them by i did
 var self = items.filter(function(item) {
   return item.who_made === "i_did";
 });
+// takes length of items that are self made 
 console.log(self.length + " were made by their sellers")
